eb3b4bb8637e5a1d02b9fa1a4b095a05554da5ad
Jess Skelton <jessskelton888@gmail.com>
2023-11-02 14:30:27 -0500

    began js code by defining functions to randomized defined variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// DOM Elements
 
-// Generate a new & secure password
+// Randomized Generator Functions into Object for easy use
+// const randomized = {
+//     lower: randomLower,
+//     upper: randomUpper,
+//     number: randomNumber,
+//     symbol: randomSymbol,
+// }
+
+// console.log(randomized.lower);
+
+// Click EventListener for Generate Button 
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
+// Generate New & Secure Password Function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,5 +25,25 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Randomized Generator Functions
+function randomLower() {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    return lower[Math.floor(Math.random() * lower.length)];
+} 
+
+function randomUpper() {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return upper[Math.floor(Math.random() * upper.length)];
+}
+
+function randomNumber() {
+    const number = "0123456789";
+    return number[Math.floor(Math.random() * number.length)];
+}
+
+function randomSymbol() {
+    const symbol = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+// console.log(randomLower);
