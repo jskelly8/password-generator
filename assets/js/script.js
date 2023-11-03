35ad09a3bf0223e6dcefc3e92d383cf427723e52
Jess Skelton <jessskelton888@gmail.com>
2023-11-02 23:07:06 -0500

    Added confirmation prompts to include certain criteria or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,34 @@
 // Assignment Code
 
 //Randomized Generator Functions into Object for easy use
-const randomized = {
-    lower: randomLower,
-    upper: randomUpper,
-    number: randomNumber,
-    symbol: randomSymbol,
-}
+// const randomized = {
+//     lower: randomLower,
+//     upper: randomUpper,
+//     number: randomNumber,
+//     symbol: randomSymbol,
+// }
 
 // Promt for Password Length desired
 const passLength = prompt("Please enter desired password length as a number between a minimum of 8 and a maximum of 128.")
 
-if (passLength < 8 || passLength > 128) {
-    alert('Please enter a valid number between 8 and 128.');
-} else {
+if (passLength > 8 && passLength < 128) {
     alert('Valid length entered: ' + passLength)
+} else {
+    alert('Please enter a valid number between 8 and 128. Please start again by refreshing the page.');
+    //------- return;
 };
 
 // Confirmation of whether or not to include lowercase, uppercase, number, &/or symbol characters
+const yesLower = confirm('Click OK to include lowercase letters, Cancel for no.')
+const yesUpper = confirm('Click OK to include uppercase letters, Cancel for no.')
+const yesNumber = confirm('Click OK to include numbers, Cancel for no.')
+const yesSymbol  = confirm('Click OK to include symbols, Cancel for no.')
 
+//Error alert if no to all 4 criteria prompts
+if (!yesLower && !yesUpper && !yesNumber && !yesSymbol) {
+    alert('At least one criteria of lowercase, uppercase, number, or symbol must be included. Please start again by refreshing the page.');
+    //------- return;
+};
 
 
 // Click EventListener for Generate Button 
@@ -27,13 +37,11 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-// Generate New & Secure Password Function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Randomized Generator Functions
